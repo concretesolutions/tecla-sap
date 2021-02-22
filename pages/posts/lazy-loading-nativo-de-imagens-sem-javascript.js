2e12f6ae7ccf1d8fb17ddab786dc9a562ea10c72
ana.paula.da.s.rocha <apdsrocha@gmail.com>
2021-02-22 18:17:58 -0300

    alters name of export default function to differ from previous post

--- pages/posts/lazy-loading-nativo-de-imagens-sem-javascript.js
+++ pages/posts/lazy-loading-nativo-de-imagens-sem-javascript.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../../styles/Home.module.css'
 
-export default function TipsJavascriptOptimization() {
+export default function NativeLazyLoading() {
   return (
     <>
       <header className={styles.header}>
