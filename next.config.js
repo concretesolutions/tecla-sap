26ffc91d32dd6297942f44cb138b0296573d4f5e
Diego Griep <fale@diegogriep.com.br>
2021-02-23 18:41:14 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa')
+const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withPWA({
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/NaveenDA/naveenda.github.io/gh-pages/' : '',
   pwa: {
     dest: 'public'
   }
