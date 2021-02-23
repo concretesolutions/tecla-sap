68aa51704809e77e50014e1bdd6d70275bd97cc7
diegogriep <fale@diegogriep.com.br>
2021-02-23 21:29:43 +0000

    Deploying to gh-pages from @ concretesolutions/tecla-sap@5872552d4f414446e80fec623f9817d553060463 🚀

--- README.md
+++
@@ -1,1 +0,0 @@
-# tecla-sap
--- next.config.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa')
-
-module.exports = withPWA({
-  pwa: {
-    dest: 'public'
-  }
-})
