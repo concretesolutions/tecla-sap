2ccbc3cb9f6d6f0d36b31d9f2da97f29cd845f6c
Diego Griep <fale@diegogriep.com.br>
2021-02-24 14:22:00 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV === 'production'
 module.exports = withPWA({
   // Use the CDN in production and localhost for development.
   assetPrefix: isProd
-    ? 'https://cdn.statically.io/gh/concretesolutions/tecla-sap/gh-pages/'
+    ? 'https://cdn.statically.io/gh/concretesolutions/tecla-sap/gh-pages'
     : '',
   pwa: {
     dest: 'public'
