e81435202e27db4a2986cf6ec17672a41b3eee4f
Diego Griep <fale@diegogriep.com.br>
2021-02-24 14:26:59 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa')
-const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withPWA({
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd
-    ? 'https://cdn.statically.io/gh/concretesolutions/tecla-sap/gh-pages'
-    : '',
   pwa: {
     dest: 'public'
   }
