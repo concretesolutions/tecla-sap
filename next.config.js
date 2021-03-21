4cc46f177d77f311ebe9cc1c72583c7e2d1b12f4
camilo micheletto <camilomichelettors@gmail.com>
2021-03-20 21:53:16 -0300

    added Sass dependecies

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa')
+const path = require('path')
 
 module.exports = withPWA({
   pwa: {
     dest: 'public'
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')]
   }
 })
